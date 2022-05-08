f3441530a2df4113b45e6cbee7f3dc92626aa229
viniciustlima <vinitlima2007.14@gmail.com>
2022-05-08 16:37:50 -0300

    first riddle and tips

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
 function redirect (lvl) {
 	location.href = `http://${location.href.split("/")[2]}/riddle/stuff/${lvl}.html`;
 }
+
+function slug(text) {
+	return text
+		.toLowerCase()
+		.replace(/[^\w ]+/g, "")
+		.replace(/ +/g, "-");
+}
